c2bdda8fa44f8c577e8970a3e42366fc05fcac8e
Siniscalchi Davide <siniscalchi.davide@iisgalvanimi.edu.it>
2026-02-06 10:00:39 +0000

    Punto 3

--- Siniscalchi-fila-A/src/app/app.ts
+++ Siniscalchi-fila-A/src/app/app.ts
@@ -1,8 +1,11 @@
 import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
---
+++ Siniscalchi-fila-A/src/app/third-component/third-component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-third-component',
+  standalone: true,
+  imports: [],
+  templateUrl: './third-component.html',
+  styleUrl: './third-component.css',
+})
+export class ThirdComponent {
+
+}
